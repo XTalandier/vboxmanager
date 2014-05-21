df160498fd49b92149c2326a950dffe13374f676
Clemens Burger <clemo@hockwien.at>
2014-05-21 16:29:35 +0200

    add remote support vbox.setRemote('hostOrIp',options)

---
+++ test/config.js
@@ -0,0 +1,10 @@
+module.exports = undefined;
+/*{
+    host: '10.8.0.X',
+    options: {
+        username: 'userrunningvbox',
+        passphrase: 'somePasswordIfAny',
+        tmpfile: './testing.txt',
+        privateKey: require('fs').readFileSync('/home/userrunningnodejs/.ssh/id_rsa')
+    }
+}*/
